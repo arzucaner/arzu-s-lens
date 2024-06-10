8528bba33472c98dcec53ad2090f46eb2c8fbe74
Arzu Caner <arzuguneycaner@gmail.com>
2024-06-10 22:27:55 +0100

    Add script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    var sidebar = document.getElementById("sidebar");
+    var main = document.getElementById("main");
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        main.style.marginLeft = "0";
+    } else {
+        sidebar.style.width = "250px";
+        main.style.marginLeft = "250px";
+    }
+}
+
